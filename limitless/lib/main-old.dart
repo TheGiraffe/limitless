d4e920a8802f7838cfbacc92d91c02552bd3f688
TheGiraffe <TheGiraffe@users.noreply.github.com>
2025-09-07 13:42:52 -1000

    Learning Dart, started putting together page for creating series of stories

--- limitless/lib/main-old.dart
+++ limitless/lib/main-old.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,9 +11,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Limitless',
+      title: 'Flutter Demo',
       theme: ThemeData(
-        fontFamily: GoogleFonts.spaceMono().fontFamily,
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
         colorScheme: ColorScheme(
           brightness: Brightness.dark,
           primary: Colors.white,
@@ -25,11 +38,9 @@ class MyApp extends StatelessWidget {
           onError: Colors.black,
           surface: Colors.black,
           onSurface: Colors.white,
-          background: Colors.black,
-          onBackground: Colors.white
           ),
       ),
-      home: const MyHomePage(title: 'Limitless (WIP)'),
+      home: const MyHomePage(title: 'Flutter Demo Home'),
     );
   }
 }
@@ -62,7 +73,7 @@ class _MyHomePageState extends State<MyHomePage> {
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter = _counter + 5;
+      _counter++;
     });
   }
 
@@ -76,8 +87,13 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
         backgroundColor: Colors.white,
         foregroundColor: Colors.black,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
@@ -99,63 +115,20 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('Welcome to Limitless'),
+            const Text('You have pushed the button this many times:'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
-            Text('Press the + button to create a series.'),
+            Text('Hello'),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          Navigator.push(
-            context,
-            MaterialPageRoute<void>(
-              builder: (context) => CreateSeriesPage(title: 'Create Series')
-              )
-          );
-        },
-        tooltip: 'Create Series',
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
-
-class CreateSeriesPage extends StatefulWidget{
-  const CreateSeriesPage({super.key, required this.title});
-  final String title;
-  @override
-  State <CreateSeriesPage> createState() => _CreateSeriesPageState();
-}
-
-class _CreateSeriesPageState extends State<CreateSeriesPage>{
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        foregroundColor: Colors.black,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              TextFormField(
-                decoration: const InputDecoration(
-                  hintText: 'Name of Series'
-                ),
-              ),
-            ],
-          ),
-        )
-      ),
     );
   }
 }
