27ebc60037db09f774daef483f266773970f96f8
TheGiraffe <TheGiraffe@users.noreply.github.com>
2025-09-07 15:59:59 -1000

    Made good progress on the basic page layout!

--- limitless/lib/main.dart
+++ limitless/lib/main.dart
@@ -26,16 +26,25 @@ class MyApp extends StatelessWidget {
           surface: Colors.black,
           onSurface: Colors.white,
           background: Colors.black,
-          onBackground: Colors.white
-          ),
+          onBackground: Colors.white,
+        ),
       ),
-      home: const MyHomePage(title: 'Limitless (WIP)'),
+      home: const MyHomePage(
+        title: 'Limitless (WIP)',
+        username: "Test user name",
+        worldname: "Test world name",
+      ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({
+    super.key,
+    required this.title,
+    required this.username,
+    required this.worldname,
+  });
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -47,33 +56,16 @@ class MyHomePage extends StatefulWidget {
   // always marked "final".
 
   final String title;
+  final String username;
+  final String worldname;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter = _counter + 5;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
@@ -81,60 +73,151 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('Welcome to Limitless'),
+            const Text('Welcome to Limitless,'),
             Text(
-              '$_counter',
+              widget.username,
               style: Theme.of(context).textTheme.headlineMedium,
             ),
-            Text('Press the + button to create a series.'),
+            Container(
+              padding: EdgeInsets.only(top: 30),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [Text("Here, you can:")],
+              ),
+            ),
+            Flexible(
+              child: ListView(
+                shrinkWrap: true,
+                children: const <Widget>[
+                  ListTile(
+                    title: Text(
+                      "- Create stories",
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  ListTile(
+                    title: Text("- Collect items", textAlign: TextAlign.center),
+                  ),
+                  ListTile(
+                    title: Text(
+                      "- Save your world",
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          Navigator.push(
-            context,
-            MaterialPageRoute<void>(
-              builder: (context) => CreateSeriesPage(title: 'Create Series')
-              )
-          );
-        },
-        tooltip: 'Create Series',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      floatingActionButton: OptionsWidget(worldname: widget.worldname),
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
 
-class CreateSeriesPage extends StatefulWidget{
-  const CreateSeriesPage({super.key, required this.title});
-  final String title;
+class OptionsWidget extends StatefulWidget {
+  const OptionsWidget({super.key, required this.worldname});
+  final String worldname;
   @override
-  State <CreateSeriesPage> createState() => _CreateSeriesPageState();
+  State<OptionsWidget> createState() => _OptionsWidgetState();
 }
 
-class _CreateSeriesPageState extends State<CreateSeriesPage>{
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+class _OptionsWidgetState extends State<OptionsWidget> {
+  final double _actionButtonPadding = 8;
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Container(
+          padding: EdgeInsets.all(_actionButtonPadding),
+          child: FloatingActionButton(
+            heroTag: "Explore",
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute<void>(
+                  builder: (context) => ExplorePage(title: 'Explore'),
+                ),
+              );
+            },
+            tooltip: 'Explore',
+            child: const Icon(Icons.rocket),
+          ),
+        ),
+        // Container(
+        //   padding: EdgeInsets.all(_actionButtonPadding),
+        //   child: FloatingActionButton(
+        //     heroTag: "MyWorld",
+        //     onPressed: () {
+        //       Navigator.push(
+        //         context,
+        //         MaterialPageRoute<void>(
+        //           builder: (context) => CreatorsPage(title: widget.worldname),
+        //         ),
+        //       );
+        //     },
+        //     tooltip: 'My World',
+        //     child: const Icon(Icons.language),
+        //   ),
+        // ),
+        Container(
+          padding: EdgeInsets.all(_actionButtonPadding),
+          child: FloatingActionButton(
+            heroTag: "Shop",
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute<void>(
+                  builder: (context) => ShopPage(title: "Shop"),
+                ),
+              );
+            },
+            tooltip: "Shop",
+            child: const Icon(Icons.store),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.all(_actionButtonPadding),
+          child: FloatingActionButton(
+            heroTag: "CreatorsHub",
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute<void>(
+                  builder: (context) => CreatorsPage(title: 'Creators Hub'),
+                ),
+              );
+            },
+            tooltip: 'Creators Hub',
+            child: const Icon(Icons.library_add),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class CreatorsPage extends StatefulWidget {
+  const CreatorsPage({super.key, required this.title});
+  final String title;
+  @override
+  State<CreatorsPage> createState() => _CreatorsPageState();
+}
+
+class _CreatorsPageState extends State<CreatorsPage> {
+  // final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final ButtonStyle btnstyle = ElevatedButton.styleFrom(
+    backgroundColor: Colors.white,
+    foregroundColor: Colors.black,
+  );
+  final double _buttonPadding = 8;
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
@@ -142,19 +225,99 @@ class _CreateSeriesPageState extends State<CreateSeriesPage>{
         title: Text(widget.title),
       ),
       body: Center(
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              TextFormField(
-                decoration: const InputDecoration(
-                  hintText: 'Name of Series'
-                ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              padding: EdgeInsets.all(_buttonPadding),
+              child: ElevatedButton(
+                style: btnstyle,
+                onPressed: () {},
+                child: const Text("Create New Series"),
               ),
-            ],
-          ),
-        )
+            ),
+            Container(
+              padding: EdgeInsets.all(_buttonPadding),
+              child: ElevatedButton(
+                style: btnstyle,
+                onPressed: () {},
+                child: const Text("Create New Story in Series"),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(_buttonPadding),
+              child: ElevatedButton(
+                style: btnstyle,
+                onPressed: () {},
+                child: const Text("Edit Existing Series"),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// Form(
+//           key: _formKey,
+//           child: Column(
+//             crossAxisAlignment: CrossAxisAlignment.start,
+//             children: <Widget>[
+//               TextFormField(
+//                 decoration: const InputDecoration(hintText: 'Name of Series'),
+//               ),
+//             ],
+//           ),
+//         ),
+
+class ExplorePage extends StatefulWidget {
+  const ExplorePage({super.key, required this.title});
+  final String title;
+  @override
+  State<ExplorePage> createState() => _ExplorePageState();
+}
+
+class _ExplorePageState extends State<ExplorePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [Text("This is the Explore Page")],
+        ),
+      ),
+    );
+  }
+}
+
+class ShopPage extends StatefulWidget {
+  const ShopPage({super.key, required this.title});
+  final String title;
+  @override
+  State<ShopPage> createState() => _ShopPageState();
+}
+
+class _ShopPageState extends State<ShopPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [Text("This is the Shop Page - people will be able to buy stuff here.")],
+        ),
       ),
     );
   }
